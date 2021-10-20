7037c91eafebb0cd931bb62da8baaf06a3d278b2
Mohammed Hasan <rupams2002@yahoo.com>
2021-10-20 12:36:03 -0400

    Initial commit

---
+++ src/main/java/com/myproject/helloworld/controller/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.myproject.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+//@RequestMapping("/hello")
+
+public class HelloWorld {
+
+	@GetMapping("/")
+	public String helloWorld() {
+		return "<b>Hello World</b>";		
+	}
+	
+}
